6e9d15aacfee272842bf53f15a3341a067834161
pdelpriore <paul.del.priore29@gmail.com>
2020-02-25 16:37:52 +0100

    add try catch block in email confirm resolver

--- graphql/resolvers/emailConfirmationResolver.js
+++ graphql/resolvers/emailConfirmationResolver.js
@@ -2,13 +2,17 @@ const User = require("../../model/User");
 const { verifyToken } = require("../operations/verifyToken");
 
 module.exports = {
-  confirmEmail: async ({ token, email }) => {
-    await verifyToken(token);
-    await User.findOneAndUpdate(
-      { email: email },
-      { $set: { isEmailConfirmed: true } },
-      { new: true }
-    ).exec();
+  confirmEmail: async ({ token, email }, { req, res }) => {
+    try {
+      await verifyToken(token);
+      await User.findOneAndUpdate(
+        { email: email },
+        { $set: { isEmailConfirmed: true } },
+        { new: true }
+      ).exec();
+    } catch (err) {
+      if (err) throw new Error(err);
+    }
     return "ok";
   }
 };
