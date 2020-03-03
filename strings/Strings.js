7cfb0c06622beb0305b45d433a69c51b82545977
pdelpriore <paul.del.priore29@gmail.com>
2020-03-03 09:14:16 +0100

    add point in end of phrases

--- strings/Strings.js
+++ strings/Strings.js
@@ -15,20 +15,20 @@ const strings = Object.freeze({
     CUSTOMER_EMAIL_SENT: "Ton email a été bien envoyé. Merci."
   },
   remindPass: {
-    PASSWORD_SENT: "nouveau mot de passe envoyé"
+    PASSWORD_SENT: "nouveau mot de passe envoyé."
   },
   errors: {
     signup: {
-      USER_EXISTS: "utilisateur déjà inscrit"
+      USER_EXISTS: "utilisateur déjà inscrit."
     },
     remindPass: {
-      USER_NULL: "utilisateur n'existe pas"
+      USER_NULL: "utilisateur n'existe pas."
     },
     validateSignupForm: {
-      NAME_LENGTH: "ton nom d'utilisateur doit contenir au moins 5 caractères",
-      NAME_SPACE: "ton nom d'utilisateur ne peut pas contenir des espaces",
-      WRONG_EMAIL: "ton email n'est pas correct",
-      EMAILS_MISMATCH: "emails saisis ne sont pas les mêmes",
+      NAME_LENGTH: "ton nom d'utilisateur doit contenir au moins 5 caractères.",
+      NAME_SPACE: "ton nom d'utilisateur ne peut pas contenir des espaces.",
+      WRONG_EMAIL: "ton email n'est pas correct.",
+      EMAILS_MISMATCH: "emails saisis ne sont pas les mêmes.",
       WRONG_PASSWORD:
         "ton mot de passe doit contenir au moins: \n 1. Une chiffre \n 2. Un caractère minuscule \n 3. Un caractère majuscule \n 4. Huit caractères"
     }
