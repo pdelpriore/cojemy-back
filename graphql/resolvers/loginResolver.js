015c5cf8314d2bf256e76f0d3e105da730475551
pdelpriore <paul.del.priore29@gmail.com>
2020-03-03 14:10:22 +0100

    send cookie to the client when user log in

--- graphql/resolvers/loginResolver.js
+++ graphql/resolvers/loginResolver.js
@@ -8,13 +8,19 @@ const {
 const { generateToken } = require("../operations/token/generateToken");
 
 module.exports = {
-  login: async ({ email, password }) => {
+  login: async ({ email, password }, { res }) => {
     try {
       const user = await isUserExist(email);
       await isEmailConfirmed(user.isEmailConfirmed);
       await isPasswordCorrect(password, user.password);
       const token = await generateToken(user.email);
-      console.log(token);
+
+      res.cookie("id", token, {
+        httpOnly: true
+        //on production set secure true
+        //secure: true
+      });
+
       return user;
     } catch (err) {
       if (err) throw new Error(err);
