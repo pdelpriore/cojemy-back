8b3d760d879bc8d72df2db9e7edda3ee2cc4bea7
pdelpriore <paul.del.priore29@gmail.com>
2020-03-09 20:25:05 +0100

    add error types

---
+++ errors/type/errorType.js
@@ -0,0 +1,18 @@
+export const errorType = {
+  USER_EXISTS: {
+    message: "utilisateur déjà inscrit.",
+    type: "email"
+  },
+  WRONG_PASSWORD: {
+    message: "mot de passe incorrect.",
+    type: "password"
+  },
+  EMAIL_UNCONFIRMED: {
+    message: "email non confirmé.",
+    type: "email"
+  },
+  LOGIN_ACCOUNT_ERROR: {
+    message: "c'est un compte google.",
+    type: ""
+  }
+};
