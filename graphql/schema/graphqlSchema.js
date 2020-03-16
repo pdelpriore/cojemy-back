c0b55558147a825ab44e4cbab2c4b500add8f2e2
pdelpriore <paul.del.priore29@gmail.com>
2020-03-16 22:03:11 +0100

    remove comments from graphql schema

--- graphql/schema/graphqlSchema.js
+++ graphql/schema/graphqlSchema.js
@@ -1,4 +1,3 @@
-const { buildSchema } = require("graphql");
 const graphql = require("graphql");
 const {
   GraphQLObjectType,
@@ -117,44 +116,3 @@ module.exports = new GraphQLSchema({
   query: RootQuery,
   mutation: RootMutation
 });
-
-// module.exports = buildSchema(
-//   `
-//     type User {
-//       _id: ID!
-//       name: String!
-//       email: String!
-//       password: String!
-//       googlePhoto: String!
-//       isEmailConfirmed: Boolean!
-//       isGoogleUser: Boolean!
-//       creationDate: String!
-//       recipes: [Recipe]
-//     }
-//     type Recipe {
-//       _id: ID!
-//       title: String!
-//       picture: String
-//       category: String!
-//       date: String!
-//       author: User!
-//     }
-//     type RootQuery {
-//         retrieveRecipes(category: String!): [Recipe!]!
-//     }
-//     type RootMutation {
-//         signUp(name: String!, email: String!, confirmEmail: String!, password: String!): User!
-//         customerContact(subject: String!, email: String!, content: String!): String
-//         remindPassword(email: String!): String
-//         login(email: String!, password: String!): User!
-//         logout(email: String!): Boolean
-//         signUpGoogleUser(name: String!, email: String!, googlePhoto: String!): User!
-//         loginGoogleUser(email: String!): User!
-//         logoutGoogleUser(email: String!): Boolean
-//     }
-//       schema {
-//         query: RootQuery
-//         mutation: RootMutation
-//       }
-//     `
-// );
