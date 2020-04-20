8231dc28aeb6d81978bfa4e90adba79010918788
pdelpriore <paul.del.priore29@gmail.com>
2020-04-20 20:56:52 +0200

    implement create stream from buffer base64 image properly

--- graphql/operations/image/uploadImage.js
+++ graphql/operations/image/uploadImage.js
@@ -1,37 +1,32 @@
 const fs = require("fs");
 const path = require("path");
 const randomstring = require("randomstring");
+const stream = require("stream");
 
 const uploadImage = async (recipeImage) => {
   const imagePath = await new Promise((resolve, reject) => {
-    fs.writeFile(
-      path.join(
-        __dirname,
-        "..",
-        "..",
-        "..",
-        "uploads",
-        "imgs",
-        "recipes",
-        `${randomstring.generate(20)}_${recipeImage.imageName}`
-      ),
-      recipeImage.image.replace(/^data:image\/\w+;base64,/, ""),
-      { encoding: "base64" },
-      (err) => {
-        if (err) console.log(err);
-        return resolve(
-          path.join(
-            __dirname,
-            "..",
-            "..",
-            "..",
-            "uploads",
-            "imgs",
-            "recipes",
-            `${randomstring.generate(20)}_${recipeImage.imageName}`
-          )
-        );
-      }
+    const bufferStream = new stream.PassThrough();
+
+    bufferStream.end(
+      new Buffer.from(
+        recipeImage.image.replace(/^data:image\/\w+;base64,/, ""),
+        "base64"
+      )
+    );
+
+    bufferStream.pipe(
+      fs.createWriteStream(
+        path.join(
+          __dirname,
+          "..",
+          "..",
+          "..",
+          "uploads",
+          "imgs",
+          "recipes",
+          `${randomstring.generate(20)}_${recipeImage.imageName}`
+        )
+      )
     );
   });
   return imagePath;
