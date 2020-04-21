3dd76b47669325af1c0c9c4a9da48b7b93e48013
pdelpriore <paul.del.priore29@gmail.com>
2020-04-21 11:00:31 +0200

    refactor code

--- graphql/operations/image/uploadImage.js
+++ graphql/operations/image/uploadImage.js
@@ -5,29 +5,28 @@ const stream = require("stream");
 
 const uploadImage = async (recipeImage) => {
   const imagePath = await new Promise((resolve, reject) => {
-    const bufferStream = new stream.PassThrough();
-
-    bufferStream.end(
-      new Buffer.from(
-        recipeImage.image.replace(/^data:image\/\w+;base64,/, ""),
-        "base64"
-      )
-    );
-
-    bufferStream.pipe(
-      fs.createWriteStream(
-        path.join(
-          __dirname,
-          "..",
-          "..",
-          "..",
-          "uploads",
-          "imgs",
-          "recipes",
-          `${randomstring.generate(20)}_${recipeImage.imageName}`
+    new stream.PassThrough()
+      .end(
+        new Buffer.from(
+          recipeImage.image.replace(/^data:image\/\w+;base64,/, ""),
+          "base64"
         )
       )
-    );
+      .pipe(
+        fs.createWriteStream(
+          path.join(
+            __dirname,
+            "..",
+            "..",
+            "..",
+            "uploads",
+            "imgs",
+            "recipes",
+            `${randomstring.generate(20)}_${recipeImage.imageName}`
+          )
+        )
+      );
+
     resolve(
       path.join(
         __dirname,
