28fec1c0fda8548a91ccf96fe80181d200f2f683
pdelpriore <paul.del.priore29@gmail.com>
2020-05-20 16:07:43 +0200

    change parameter name

--- graphql/operations/image/uploadImage.js
+++ graphql/operations/image/uploadImage.js
@@ -4,14 +4,14 @@ const randomstring = require("randomstring");
 const stream = require("stream");
 const { strings } = require("../../../strings/Strings");
 
-const uploadImage = async (recipeImage, type) => {
+const uploadImage = async (imageObj, type) => {
   const imagePath = await new Promise((resolve, reject) => {
-    const imageName = `${randomstring.generate(20)}-${recipeImage.imageName}`;
+    const imageName = `${randomstring.generate(20)}-${imageObj.imageName}`;
 
     new stream.PassThrough()
       .end(
         new Buffer.from(
-          recipeImage.image.replace(/^data:image\/\w+;base64,/, ""),
+          imageObj.image.replace(/^data:image\/\w+;base64,/, ""),
           "base64"
         )
       )
