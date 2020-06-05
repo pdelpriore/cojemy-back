a956160c4fc896ceda6b504a22390f7dc076cc2f
pdelpriore <paul.del.priore29@gmail.com>
2020-06-05 15:10:28 +0200

    remove a promise is email confirm

--- graphql/resolvers/login/loginResolver.js
+++ graphql/resolvers/login/loginResolver.js
@@ -2,9 +2,6 @@ const { isUserExist } = require("../../operations/verification/isUserExist");
 const {
   isPasswordCorrect,
 } = require("../../operations/verification/isPasswordCorrect");
-const {
-  isEmailConfirmed,
-} = require("../../operations/verification/isEmailConfirmed");
 const { generateToken } = require("../../operations/token/generateToken");
 const {
   protectAgainstHack,
@@ -18,16 +15,19 @@ module.exports = {
       await protectAgainstHack(req);
       const user = await isUserExist(email);
       if (!user.isGoogleUser) {
-        await isEmailConfirmed(user.isEmailConfirmed);
-        await isPasswordCorrect(password, user.password);
-        const token = await generateToken(user.email);
-        res.cookie("id", token, {
-          httpOnly: true,
-          //on production set secure true
-          //secure: true
-        });
+        if (user.isEmailConfirmed) {
+          await isPasswordCorrect(password, user.password);
+          const token = await generateToken(user.email);
+          res.cookie("id", token, {
+            httpOnly: true,
+            //on production set secure true
+            //secure: true
+          });
 
-        return hideUserPassword(user);
+          return hideUserPassword(user);
+        } else {
+          throw new Error(strings.errors.login.EMAIL_UNCONFIRMED);
+        }
       } else {
         throw new Error(strings.errors.login.ERROR);
       }
