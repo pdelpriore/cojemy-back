1b2dca3f2a1b41a1cc83500ccabb07600bc6dc96
pdelpriore <paul.del.priore29@gmail.com>
2020-06-27 11:53:07 +0200

    fix checking if event reserved

--- graphql/operations/verification/isEventReserved.js
+++ graphql/operations/verification/isEventReserved.js
@@ -1,5 +1,6 @@
 const Event = require("../../../model/Event");
 const Address = require("../../../model/Address");
+const moment = require("moment");
 const { strings } = require("../../../strings/Strings");
 
 const isEventReserved = (title, eventDate, addressObj) => {
@@ -8,20 +9,22 @@ const isEventReserved = (title, eventDate, addressObj) => {
       const eventExists = await Event.findOne({
         title: title,
       });
-      const eventReserved = await Event.findOne({ eventDate: eventDate });
-      const addressReserved =
-        eventReserved &&
-        (await Address.findOne({
-          event: eventReserved._id,
-          streetNumber: addressObj.streetNumber,
-          streetName: addressObj.streetName,
-          city: addressObj.city,
-          country: addressObj.country,
-        }));
+
+      const addressReserved = await Address.findOne({
+        streetNumber: addressObj.streetNumber,
+        streetName: addressObj.streetName,
+        city: addressObj.city,
+        country: addressObj.country,
+      });
+      const eventReserved =
+        addressReserved && (await Event.findById(addressReserved.event));
 
       if (eventExists) {
         reject(strings.errors.addNewEvent.EVENT_EXISTS);
-      } else if (addressReserved) {
+      } else if (
+        eventReserved &&
+        moment(eventReserved.eventDate).isSame(eventDate)
+      ) {
         reject(strings.errors.addNewEvent.EVENT_RESERVED);
       } else {
         resolve();
