04553c685528367dba711bbe876b84bc2ce1e192
pdelpriore <paul.del.priore29@gmail.com>
2020-07-24 12:41:07 +0200

    little prettify

--- Server.js
+++ Server.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-const { dbConnection } = require("./config/db/dbConnection");
-const { setGraphQL } = require("./middlewares/setGraphQL/setGraphQL");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const schedule = require("node-schedule");
+const { dbConnection } = require("./config/db/dbConnection");
+const { setGraphQL } = require("./middlewares/setGraphQL/setGraphQL");
+const ioConnection = require("./socketIo/connection/ioConnection");
+const {
+  userAuthGraphQL,
+} = require("./middlewares/userAuth/graphQL/userAuthGraphQL");
+const {
+  userAuthSocketIO,
+} = require("./middlewares/userAuth/socketIO/userAuthSocketIO");
 const { strings } = require("./strings/Strings");
 const { capitalizeFirst } = require("./util/Util");
 const emailConfirmation = require("./routes/emailConfirmation");
@@ -18,13 +25,6 @@ const renderHereMap = require("./routes/renderHereMaps");
 const generateGoogleAuthUrl = require("./helpers/generateGoogleAuthUrl");
 const checkRequest = require("./util/checkRequest");
 const { removeUnconfirmedUsers } = require("./util/removeUnconfirmedUsers");
-const ioConnection = require("./socketIo/connection/ioConnection");
-const {
-  userAuthGraphQL,
-} = require("./middlewares/userAuth/graphQL/userAuthGraphQL");
-const {
-  userAuthSocketIO,
-} = require("./middlewares/userAuth/socketIO/userAuthSocketIO");
 
 app.use(
   cors({
