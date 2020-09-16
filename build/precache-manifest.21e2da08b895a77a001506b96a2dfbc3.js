390fe6ecd5601ecd7a35d94c683159605cb363f4
pdelpriore <paul.del.priore29@gmail.com>
2020-09-16 15:07:35 +0200

    add build

---
+++ build/precache-manifest.21e2da08b895a77a001506b96a2dfbc3.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065c79b468d67e43e04b41b997ba1234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ae1531aa292f80ce276",
+    "url": "/static/css/2.e61fc129.chunk.css"
+  },
+  {
+    "revision": "65e8404fba3427ce27e6",
+    "url": "/static/css/main.6657fe4d.chunk.css"
+  },
+  {
+    "revision": "4ae1531aa292f80ce276",
+    "url": "/static/js/2.62a0d752.chunk.js"
+  },
+  {
+    "revision": "fcdc5e536d87d4615bb78a0d5fa80be3",
+    "url": "/static/js/2.62a0d752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e8404fba3427ce27e6",
+    "url": "/static/js/main.ba17dcd7.chunk.js"
+  },
+  {
+    "revision": "cdf041b3892fd22a989b",
+    "url": "/static/js/runtime-main.6c427cc5.js"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/static/media/Nunito-Regular.d2e691bc.ttf"
+  },
+  {
+    "revision": "76ae20d4d4b7d46e613d2581512fcf73",
+    "url": "/static/media/Nunito-SemiBold.76ae20d4.ttf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "81f085e68cd9915003dd396105d2004a",
+    "url": "/static/media/contactret.81f085e6.jpg"
+  },
+  {
+    "revision": "812972653055d8a49d4f3c7219d837f0",
+    "url": "/static/media/cookbookret.81297265.jpg"
+  },
+  {
+    "revision": "cdff214d44a837dccd062ba9ecb3cfd1",
+    "url": "/static/media/cookerret.cdff214d.png"
+  },
+  {
+    "revision": "a744b51b7b5998078ae6a661ce8d0e4a",
+    "url": "/static/media/cookfriendsret.a744b51b.jpg"
+  },
+  {
+    "revision": "745f56d734b9f7e289e3a9d61513b21f",
+    "url": "/static/media/eventret.745f56d7.jpg"
+  },
+  {
+    "revision": "a3acf0c424607c335ec85bc93ec5253c",
+    "url": "/static/media/facebook2ret.a3acf0c4.jpg"
+  },
+  {
+    "revision": "3d5fcac7b584942f6fe477e3eab268fc",
+    "url": "/static/media/googleButton.3d5fcac7.jpg"
+  },
+  {
+    "revision": "426aea2d0d7c2eaed9e708d352d92029",
+    "url": "/static/media/instagramret.426aea2d.jpg"
+  },
+  {
+    "revision": "cecd179982ca217b71d76698ae56efb2",
+    "url": "/static/media/knobovenret.cecd1799.jpg"
+  },
+  {
+    "revision": "9d9bf0c8ca21099e598cb5ebbf8c8029",
+    "url": "/static/media/knobovenretactive.9d9bf0c8.jpg"
+  },
+  {
+    "revision": "72591987835c42c314653883ad9ae1a5",
+    "url": "/static/media/loginret.72591987.jpg"
+  },
+  {
+    "revision": "609d52b262b0cf57604d954e89b1814e",
+    "url": "/static/media/panret.609d52b2.jpg"
+  },
+  {
+    "revision": "2105b5cf582442de40036be758b565c1",
+    "url": "/static/media/panret2.2105b5cf.jpg"
+  },
+  {
+    "revision": "82e35d0b841b664b0bff5cda91224a76",
+    "url": "/static/media/recipebookret.82e35d0b.png"
+  },
+  {
+    "revision": "f5af451ca3e4a3da5ce196df0c309a68",
+    "url": "/static/media/reciperet.f5af451c.jpg"
+  },
+  {
+    "revision": "eef7a58863647d93873b6aca2a32c854",
+    "url": "/static/media/signupret.eef7a588.png"
+  },
+  {
+    "revision": "b0e79d37d4e91520f4cddbb7cfbd9591",
+    "url": "/static/media/soupret.b0e79d37.jpg"
+  },
+  {
+    "revision": "7760c9c99d419b04fdeef14f7b87c218",
+    "url": "/static/media/spoonret.7760c9c9.jpg"
+  },
+  {
+    "revision": "748f094cb2cc5e925227f9b53f8c0395",
+    "url": "/static/media/tastingret.748f094c.jpg"
+  },
+  {
+    "revision": "2212f559d9c4ae1697b33e1b136e3c82",
+    "url": "/static/media/womancookingbackg.2212f559.jpg"
+  }
+]);
